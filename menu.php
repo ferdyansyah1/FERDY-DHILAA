c1e404e77d4cb270a0eeadf247f976ef28c674a7
ferdyansyah1 <164852109+ferdyansyah1@users.noreply.github.com>
2024-06-27 22:33:33 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,12 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<body>
+<ul type="circle">
+<li><a href="inputbarang.php" target="sarinem">Input Barang</a></li> <li><a href="tampilbarang.php" target="sarinem">Tampil Barang</a></li> <li><a href="inputpelanggan.php" target="sarinem">Input pelanggan</a></li> <li><a href="tampilpelanggan.php" target="sarinem">Tampil pelanggan</a></li> <li><a href="inputpenjualan.php" target="sarinem">Input Penjualan</a></li> <li><a href="tampilpenjualan.php" target="sarinem">Hasil Penjualan</a></li>
+<li><a href="tampildetailpenjualan.php" target="sarinem">Detail Hasil Penjualan</a></li> 
+</ul>
+</body>
+ </html>
