6d55e76c5cdc6a88be1baa192ea3aeeeb09aa774
ferdyansyah1 <164852109+ferdyansyah1@users.noreply.github.com>
2024-06-27 22:32:39 +0700

    Add files via upload

---
+++ WebDila/judul.php
@@ -0,0 +1,8 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <title>Untitled Document</title>
+</head>
+<body bgcolor="#00FFFF">
+<h2><center>Selamat Datang di Toko Online F & F</center></h2>
+</body>
+</html>
